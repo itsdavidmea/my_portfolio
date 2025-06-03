0cbb38b9271dc40f5335ff7c2f8e54459142572e
DavidMEa <mea.david29@outlook.com>
2025-06-03 21:37:25 +0400

    finalized about me section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
 /* Code to change active page (the page in which the user is in)*/
 
-let navLinks = document.querySelectorAll('nav ul li a'); // this will select all the navlinks in the sidebar
+let navLinks = document.querySelectorAll('a.inner-link'); // this will select all the navlinks in the sidebar
 
 navLinks.forEach((item) => {
    
     item.addEventListener('click', function() {
+        console.log(item)
         document.querySelector('nav ul li a.active').classList.remove('active') // remove class active from current active class when clicked
-        item.classList.add('active') // add class active to the element that is being clicked 
+        // the section related to that href
+        document.querySelector(`nav ul li a[href='${item.getAttribute('href')}']`).classList.add('active') // add class active to the element that is being clicked 
         document.querySelector('main > section.active').classList.remove('active') // remove active class to the current active section 
         document.querySelector(`main > section${item.getAttribute('href')}`).classList.add('active'); // get item's href attribute e.g "#about_me"
        
